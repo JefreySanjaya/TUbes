78e2680a717de29fc84569a93b86c6a6cf02c43f
Mauritia <mauritiamona@gmail.com>
2022-10-02 21:20:28 +0700

    UGD_Push

--- app/src/main/java/com/example/ugd/Home.kt
+++ app/src/main/java/com/example/ugd/Home.kt
@@ -1,6 +1,6 @@
 package com.example.ugd
 
-import android.app.Activity
+import android.app.*
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,12 +10,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.RemoteInput
 
 class Home : AppCompatActivity() {
 
     lateinit var bottomNav : BottomNavigationView
+
+    //private val CHANNEL_ID_2 = "channel_notification_02"
+    val channelId = "My_Channel_ID"
+    val notificationId = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,9 +53,11 @@ class Home : AppCompatActivity() {
                         .setPositiveButton("Yes",object : DialogInterface.OnClickListener{
                             override fun onClick(dialogInterface: DialogInterface, i:Int) {
                                 finishAndRemoveTask()
+                                createNotificationChannel(String())
                             }
                         })
                         .show()
+
                 }
             }
         }
@@ -59,5 +69,75 @@ class Home : AppCompatActivity() {
         transaction.commit()
     }
 
+    private fun createNotificationChannel(channelId:String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "My Channel"
+            val channelDescription = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(channelId,name,importance)
+            channel.apply {
+                description = channelDescription
+            }
+
+            // Finally register the channel with system
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            // Create an instance of remote input builder
+            val remoteInput: RemoteInput = RemoteInput.Builder("KEY_TEXT_REPLY")
+                .run {
+                    setLabel("Write your message here")
+                    build()
+                }
+
+            // Create an intent
+            val intent = Intent(this, NotificationReceiver::class.java)
+            intent.action = "REPLY_ACTION"
+            intent.putExtra("KEY_NOTIFICATION_ID", notificationId)
+            intent.putExtra("KEY_CHANNEL_ID", channelId)
+            intent.putExtra("KEY_MESSAGE_ID", 1)
+
+            // Create a pending intent for the reply button
+            val replyPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                this,
+                101,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+            // Create reply action and add the remote input
+            var action: NotificationCompat.Action = NotificationCompat.Action.Builder(
+                R.drawable.ic_reply_icon,
+                "Reply",
+                replyPendingIntent
+            ).addRemoteInput(remoteInput)
+                .setAllowGeneratedReplies(true)
+                .build()
+
+
+            // Build a notification and add the action
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setContentTitle("ThankYou!")
+                .setContentText("Hello, Thanks for being here. Because of you, I get to do my dream work. I wake up each day with one mission: to help you see the beauty in you that I see. ")
+                .setStyle(NotificationCompat.BigTextStyle().bigText("Hello, Thanks for being here. Because of you, I get to do my dream work. I wake up each day with one mission: to help you see the beauty in you that I see."))
+                .setColor(Color.BLUE)
+                .addAction(action)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+            // Finally, issue the notification
+            NotificationManagerCompat.from(this).apply {
+                notify(notificationId, builder.build())
+            }
+        }
+    }
+
+
+}
+
+    private fun sendNotification2(){
 
 }
--- app/src/main/java/com/example/ugd/NotificationReceiver.kt
+++ app/src/main/java/com/example/ugd/NotificationReceiver.kt
@@ -1,13 +1,45 @@
 package com.example.ugd
 
+import android.app.RemoteInput
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
-class NotificationReceiver : BroadcastReceiver(){
-    override fun onReceive(context: Context?, intent: Intent){
-        val message = intent.getStringExtra("toastMessage")
-        Toast.makeText(context,message,Toast.LENGTH_LONG).show()
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.apply {
+            if ("REPLY_ACTION".equals(action)){
+                val message = replyMessage(this)
+                val messageId = getIntExtra("KEY_MESSAGE_ID",0)
+                Toast.makeText(context,"$messageId : $message",Toast.LENGTH_LONG).show()
+            }
+
+            context?.apply {
+                val notificationId = getIntExtra("KEY_NOTIFICATION_ID",0)
+                val channelId = getStringExtra("KEY_CHANNEL_ID")
+
+                // Build a notification and add the action
+                val builder = NotificationCompat.Builder(this, channelId.toString())
+                    .setSmallIcon(R.drawable.ic_send_24)
+                    .setContentTitle("Successful")
+                    .setContentText("Message sent!")
+
+                // Finally, issue the notification
+                NotificationManagerCompat.from(this).apply {
+                    notify(notificationId, builder.build())
+                }
+            }
+        }
+    }
+
+
+    private fun replyMessage(intent: Intent): CharSequence? {
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        return remoteInput?.getCharSequence("KEY_TEXT_REPLY")
     }
 }
+
